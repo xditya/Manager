6c9711cf929a5628f98753db95291bca5af45746
xditya <58950863+xditya@users.noreply.github.com>
2020-07-19 17:15:29 +0530

    Update misc.py

--- Manager/modules/helper_funcs/misc.py
+++ Manager/modules/helper_funcs/misc.py
@@ -4,7 +4,7 @@ from typing import List, Dict
 from telegram import MAX_MESSAGE_LENGTH, InlineKeyboardButton, Bot, ParseMode,Update
 from telegram.error import TelegramError
 
-from tg_bot import LOAD, NO_LOAD
+from Manager import LOAD, NO_LOAD
 
 
 class EqInlineKeyboardButton(InlineKeyboardButton):
