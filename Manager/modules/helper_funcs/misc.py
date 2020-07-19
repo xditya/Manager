b341677cb8455b7cf2120859b68a09440c88ec17
xditya <58950863+xditya@users.noreply.github.com>
2020-07-19 17:14:47 +0530

    Update misc.py

--- Manager/modules/helper_funcs/misc.py
+++ Manager/modules/helper_funcs/misc.py
@@ -1,10 +1,10 @@
+from math import ceil
 from typing import List, Dict
 
-from math import ceil
-from telegram import MAX_MESSAGE_LENGTH, InlineKeyboardButton, Bot, ParseMode
+from telegram import MAX_MESSAGE_LENGTH, InlineKeyboardButton, Bot, ParseMode,Update
 from telegram.error import TelegramError
 
-from Manager import NO_LOAD
+from tg_bot import LOAD, NO_LOAD
 
 
 class EqInlineKeyboardButton(InlineKeyboardButton):
@@ -62,6 +62,20 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
     elif calc == 2:
         pairs.append((modules[-1], ))
 
+    max_num_pages = ceil(len(pairs) / 10)
+    modulo_page = page_n % max_num_pages
+
+    # can only have a certain amount of buttons side by side
+    if len(pairs) > 7:
+        pairs = pairs[modulo_page * 10:10 * (modulo_page + 1)] + [
+            (EqInlineKeyboardButton("◀️", callback_data="{}_prev({})".format(prefix, modulo_page)),
+                EqInlineKeyboardButton("⏹", callback_data="bot_start"),
+             EqInlineKeyboardButton("▶️", callback_data="{}_next({})".format(prefix, modulo_page)))]
+
+    else:
+        pairs += [[EqInlineKeyboardButton("Home", callback_data="bot_start")]]
+
+
 
     return pairs
 
@@ -102,6 +116,12 @@ def revert_buttons(buttons):
 
     return res
 
+def sendMessage(text: str, bot: Bot, update: Update):
+    return bot.send_message(update.message.chat_id,
+                                    reply_to_message_id=update.message.message_id,
+                                    text=text, parse_mode=ParseMode.HTML)
+
+
 
 def is_module_loaded(name):
-    return name not in NO_LOAD
+     return name not in NO_LOAD
