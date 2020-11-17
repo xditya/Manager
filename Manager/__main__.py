2623e307ec7b02fc4b094e8fe2165f11790b46e3
RoshanHacker <74089939+RoshanHacker@users.noreply.github.com>
2020-11-17 10:05:22 +0530

    Update __main__.py

--- Manager/__main__.py
+++ Manager/__main__.py
@@ -24,7 +24,9 @@ Hello {}, my name is {}!
 
 You know how hard it is sometimes to manage group so here is the solution for you.
 
-My owner is [Aditya 🇮🇳](t.me/xditya)
+My owner is [Roshan Hacker](t.me/Godof_Hackers)
+
+My all Updates available here @Indianofficialbotsupport
 
 Click /help or Help button below to find out more about how to use me to my full potential.
 """
